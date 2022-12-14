4f28ab99bce99ae75365bc0b132c3ab97911c154
Jesica <jesica_contigiani@yahoo.com.ar>
2022-12-14 11:50:27 -0300

    Header y Footer modificados por JS

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/*letEncabezado=` <div class="Encabezado">
+let cad=`<div class="Encabezado">
 <span id="Logo" class="animate__animated animate__flip"><img src="./Imagenes/hidroponia.png" alt="Logo Hidroponia"></span>
 <h1>Cultivos Hidropónicos</h1>
 <span id="Logo1" class="animate__animated animate__flip"><img src="./Imagenes/hidroponia.png" alt="Logo Hidroponia"></span>
@@ -13,5 +13,22 @@
     <a class="nav-enlace" href="./Carrito.html">Carrito</a>
                                          
 </Nav>
-</div>`
-document.write(Encabezado)*/
+</div>
+`
+document.getElementById("Encabezado").innerHTML=cad
+
+let cad2=`<nav>
+<h5 class="textofoot">Sitio desarrollado para Cultivos Hidropónicos</h5>
+<span><small>© 2022 <code translate="no">Cultivos Hidropónicos</span></code>
+<br>
+<a  target="_blank"  href="https://facebook.com">
+    <img width="20px" src="./Imagenes/LogoFacebook.jpg" alt="Facebook">
+</a>
+<a target="_blank" href="https://www.instagram.com/">
+    <img width="20px" src="./Imagenes/LogoInstagram.jpg" alt="instagram">
+</a>
+<a target="_blank" href="https://api.whatsapp.com/send?phone=0543874564252">
+    <img width="20px" src="./Imagenes/LogoWhatsapp.jpg" alt="whatsapp">
+</a>
+</nav>`
+document.getElementById("Foot").innerHTML=cad2
